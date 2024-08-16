60e320c2b43c58549e5ebebc7c076f59bb5acd0c
sara-gnani <163330426+sara-gnani@users.noreply.github.com>
2024-08-16 15:49:39 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$GIT_BRANCH" == "dev" ];
+then
+        ./build.sh
+docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
+docker tag app:v1 sara1327/dev
+docker push sara1327/dev
+elif [ "$GIT_BRANCH" == "prod" ];
+then
+        ./build.sh
+docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
+docker tag app:v1 sara1327/prod
+docker push sara1327/prod
+else
+        echo "Deployment failed"
+fi
