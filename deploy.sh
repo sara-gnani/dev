f2016edf3827050544f32a1daa10711c133e475e
sara-gnani <163330426+sara-gnani@users.noreply.github.com>
2024-08-16 15:58:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 if [ "$GIT_BRANCH" == "dev" ];
 then
         ./build.sh
-docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
-docker tag app:v1 sara1327/dev
-docker push sara1327/dev
+        docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
+        docker tag app:v1 sara1327/dev
+        docker push sara1327/dev
 elif [ "$GIT_BRANCH" == "main" ];
 then
         ./build.sh
-docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
-docker tag app:v1 sara1327/prod
-docker push sara1327/prod
+        docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
+        docker tag app:v1 sara1327/prod
+        docker push sara1327/prod
 else
         echo "Deployment failed"
 fi
