0b664f77891228129047def733d7c5a09d611f7a
sara-gnani <163330426+sara-gnani@users.noreply.github.com>
2024-08-16 15:52:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ then
 docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
 docker tag app:v1 sara1327/dev
 docker push sara1327/dev
-elif [ "$GIT_BRANCH" == "prod" ];
+elif [ "$GIT_BRANCH" == "main" ];
 then
         ./build.sh
 docker login -u sara1327 -p dckr_pat_P3meXCrtcIeld4ILwzHVCysCrmI
